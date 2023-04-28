6441aa18b120ebd16545d86ad3ea35f20bc61ca1
Sonia William <williamlaw2017@outlook.com>
2023-04-28 15:30:25 +0800

    allow change size of board

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.insertAdjacentElement("beforeend", square);
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "blue";
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    populateBoard(input);
 }
