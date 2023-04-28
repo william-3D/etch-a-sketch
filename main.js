64e1a4db5cbb5cbb31aa3d808d685c1ccc757b61
Sonia William <williamlaw2017@outlook.com>
2023-04-28 15:38:08 +0800

    allow user to change amount of squares

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function populateBoard(size) {
     let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < 256; i++) {
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
         let square = document.createElement("div");
         square.style.backgroundColor = "blue";
         board.insertAdjacentElement("beforeend", square);
