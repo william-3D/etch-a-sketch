71430e9985fa28ca8b8a5b3641deb1482fd6422f
Sonia William <williamlaw2017@outlook.com>
2023-04-29 13:57:14 +0800

    add functionality to reset button

--- main.js
+++ main.js
@@ -37,3 +37,9 @@ function colorSquare() {
 function changeColor(choice) {
     color = choice;
 }
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => (div.style.backgroundColor = "#efe6f0"));
+}
